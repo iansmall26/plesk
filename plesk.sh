2eb221ba1f3adb63eb332c03b16cb8d717d7fc44
Ian Small <121866422+iansmall26@users.noreply.github.com>
2024-01-22 17:46:29 +0300

    Update plesk.sh

--- plesk.sh
+++ plesk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget http://installer.plesk.com/plesk-installer -O - | sh /dev/stdin --source http://installer.plesk.com/ --target /tmp/plesk-installation --select-product-id plesk --select-release-latest --installation-type "Typical" --notify-email cloudpapke@gmail.com
+wget http://installer.plesk.com/plesk-installer -O - | sudo sh /dev/stdin --source http://installer.plesk.com/ --target /tmp/plesk-installation --select-product-id plesk --select-release-latest --installation-type "Typical" --notify-email cloudpapke@gmail.com
 
